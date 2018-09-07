93f54e1bf7f15cecdc940906e181f9ef756f2c73
jra001k <jra001k@gmail.com>
2018-09-07 11:09:58 +0200

    Map tar file to 'Directory' type.

--- lib/galaxy/tools/parameters/basic.py
+++ lib/galaxy/tools/parameters/basic.py
@@ -2435,6 +2435,8 @@ parameter_types = dict(
     field=FieldTypeToolParameter,
     drill_down=DrillDownSelectToolParameter
 )
+#directory=DataToolParameter
+#directory=FileToolParameter
 
 
 def runtime_to_json(runtime_value):
