c27e5b552f3cd8457f781b5713689ac98bdf65c6
John Chilton <jmchilton@gmail.com>
2019-11-18 21:17:23 +0100

    Remove some legacy cwltool support.

--- lib/galaxy/tool_util/cwl/schema.py
+++ lib/galaxy/tool_util/cwl/schema.py
@@ -56,10 +56,7 @@ class SchemaLoader(object):
     def tool(self, **kwds):
         # cwl.workflow.defaultMakeTool() method was renamed to default_make_tool() in
         # https://github.com/common-workflow-language/cwltool/commit/886a6ac41c685f20d39e352f9c657e59f3312265
-        try:
-            default_make_tool = workflow.default_make_tool
-        except AttributeError:
-            default_make_tool = workflow.defaultMakeTool
+        default_make_tool = workflow.default_make_tool
         process_definition = kwds.get("process_definition", None)
         if process_definition is None:
             raw_process_reference = kwds.get("raw_process_reference", None)
@@ -69,25 +66,15 @@ class SchemaLoader(object):
 
         args = {"strict": self._strict, "do_validate": False}
         make_tool = kwds.get("make_tool", default_make_tool)
-        if LoadingContext is not None:
-            args["construct_tool_object"] = make_tool
-            loading_context = LoadingContext(args)
-            tool = load_tool.make_tool(
-                process_definition.document_loader,
-                process_definition.avsc_names,
-                process_definition.metadata,
-                process_definition.raw_process_reference.uri,
-                loading_context,
-            )
-        else:
-            tool = load_tool.make_tool(
-                process_definition.document_loader,
-                process_definition.avsc_names,
-                process_definition.metadata,
-                process_definition.raw_process_reference.uri,
-                make_tool,
-                args
-            )
+        args["construct_tool_object"] = make_tool
+        loading_context = LoadingContext(args)
+        tool = load_tool.make_tool(
+            process_definition.document_loader,
+            process_definition.avsc_names,
+            process_definition.metadata,
+            process_definition.raw_process_reference.uri,
+            loading_context,
+        )
         return tool
 
 
