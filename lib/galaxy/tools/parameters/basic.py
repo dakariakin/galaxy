f43e70c077b1aa44ff0cbca18f3dc95a00099a13
John Chilton <jmchilton@gmail.com>
2020-01-01 10:54:27 -0500

    Rebase with fix into CWL.

--- lib/galaxy/tools/parameters/basic.py
+++ lib/galaxy/tools/parameters/basic.py
@@ -2396,10 +2396,13 @@ class FieldTypeToolParameter(ToolParameter):
 
     def value_to_basic(self, value, app, use_security=False):
         log.info("value_to_basic of %s (%s)" % (value, type(value)))
+        if is_runtime_value(value):
+            return runtime_to_json(value)
+
         if value is None:
             return None
 
-        assert isinstance(value, dict)
+        assert isinstance(value, dict), "value [%s] is not valid for [%s]" % (value, self)
         assert "src" in value
         src = value["src"]
         if src in ["hda", "hdca", "dce"]:
